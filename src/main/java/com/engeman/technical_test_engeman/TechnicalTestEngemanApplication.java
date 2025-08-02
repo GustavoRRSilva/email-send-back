efff6bf11b2fc446a2f53984c7e431ca3e1e08ec
Gustavo <gstvrrds@gmail.com>
2025-08-02 14:05:18 -0300

    feat:first comit

---
+++ src/main/java/com/engeman/technical_test_engeman/TechnicalTestEngemanApplication.java
@@ -0,0 +1,13 @@
+package com.engeman.technical_test_engeman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestEngemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalTestEngemanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engeman/technical_test_engeman/TechnicalTestEngemanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engeman.technical_test_engeman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestEngemanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
